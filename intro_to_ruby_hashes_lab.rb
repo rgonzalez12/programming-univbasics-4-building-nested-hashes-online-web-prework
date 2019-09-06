dc1301f40776aa99289e8bd9ba2423ef7d97c4bb
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-06 19:24:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -79,15 +79,15 @@ def monopoly_with_fourth_tier
         {
           "mortgage_value"=>"$100"
         },
-        :pennsylvania_railroad=>
+      :pennsylvania_railroad=>
         {
           "mortgage_value"=>"$200"
         },
-        :b_and_o_railroad=>
+      :b_and_o_railroad=>
         {
           "mortgage_value"=>"$400"
         },
-        :shortline=>
+      :shortline=>
         {
           "mortgage_value"=>"$800"
         },
